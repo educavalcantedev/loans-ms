bdfd90b35b62e4bf118a4ee89903d96ffc139a4f
cavalcante <educavalcantedev@gmail.com>
2024-05-23 13:55:36 -0300

    Changes to work with Spring Cloud Bus and Cloud Config

--- src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
+++ src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -2,9 +2,18 @@ package com.eazybytes.loans.dto;
 
 import java.util.List;
 import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class LoansContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
